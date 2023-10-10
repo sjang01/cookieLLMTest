3b2ee2dedea7f2fc3246fe96a37aa2737a24dedc
sjang01 <sjang01@naver.com>
2023-10-10 13:38:49 +0900

    setup update

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+import setuptools
+
+
+def requirements():
+    with open(os.path.join(os.path.dirname(__file__), 'requirements.txt'), encoding='utf-8') as f:
+        return f.read().splitlines()
+
+
+setuptools.setup(
+    name="cookieLLMTest",
+    version="1.0.1",
+    license='MIT',
+    author="sjang01",
+    author_email="sjang01@naver.com",
+    description="LLM Processing test",
+    long_description=open('README.md').read(),
+    url="https://github.com/sjang01/cookieLLMTest",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    package_data={
+        'cookieLLMTest.nlpbook.classification': ['*.html'],
+        'cookieLLMTest.nlpbook.ner': ['*.html'],
+        'cookieLLMTest.nlpbook.qa': ['*.html'],
+        'cookieLLMTest.nlpbook.paircls': ['*.html'],
+        'cookieLLMTest.nlpbook.generation': ['*.html'],
+    },
+    install_requires=requirements(),
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
